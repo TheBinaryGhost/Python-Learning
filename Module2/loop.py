ce042ccaeba55a299b86686caab68c4251ca445f
Prem Kumar Sah <themr.shah108@gmail.com>
2025-09-07 14:32:34 +0545

    day 11

---
+++ Module2/loop.py
@@ -0,0 +1,8 @@
+#Looping statements
+#So first of all what is looping statement?
+#Looping statements in Python are used to execute a block of code repeatedly for a certain number
+#of times or until a specific condition is met.
+#Loop control statements change execution from its normal sequence.
+#When execution leaves a scope, all automatic objects that were created in that scope are destroyed.
+#The most common looping statements in Python are for and while loops.
+
