273497d36cfe72277967b5bd8e26d90bba4066b0
Prem Kumar Sah <themr.shah108@gmail.com>
2025-12-09 16:02:16 +0545

    Update mathmod.py

--- Module4/mathmod.py
+++ Module4/mathmod.py
@@ -7,3 +7,24 @@
 # sqrt() - returns the square root of a given number.
 # factorial() - returns the factorial of a given number.
 
+#methods of importing math module:
+# from math import function_name - to import specific functions from the math module
+
+#Factorial using math module
+from math import factorial
+print(factorial(5))  # Prints the factorial of 5
+
+import math
+print(math.factorial(2)) # Prints the factorial of 2 using math module prefix
+
+#Ceil function using math module
+import math
+print("The value is:", math.ceil(4.2))  # Prints the smallest integer greater than or equal to 4.2
+print("The value is:", math.ceil(-5.7))  # Prints the smallest integer greater than or equal to -5.7
+
+#Floor function using math module
+import math
+print("The value is:", math.floor(4.7))  # Prints the largest integer less than or equal to 4.7
+print("The value is:", math.floor(-5.3))  # Prints the largest integer less than or equal to -5.3
+
+
